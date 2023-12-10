999184c1a44dc6797bae3d21ed66724c6989678e
Ritika Ramchandani <rira3427@colorado.edu>
2023-12-09 22:22:08 -0700

    Added HPRA

--- mmu.c
+++ mmu.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <limits.h>
 
 #define PAGE_SIZE 4096
-#define NUM_FRAMES 3
+#define NUM_FRAMES 4
 #define PHYSICAL_MEMORY_SIZE (NUM_FRAMES * PAGE_SIZE)
 #define PROC_PAGES (20)
-#define DEBUG
+// #define DEBUG
 
 #ifdef DEBUG
 #define LOG(...) printf(__VA_ARGS__)
@@ -18,12 +19,13 @@ enum algorithm
 {
     FIFO = 0,
     LRU,
-    OPT
+    OPT,
+    HRPA
 };
 
 int firstIdx = 0;
 
-enum algorithm pra = OPT;
+enum algorithm pra = LRU;
 
 int page_faults = 0;
 
@@ -34,6 +36,15 @@ typedef struct
     int last_accessed; // For LRU algorithm
     int future_access; // For OPT algorithm
 } PageTableEntry;
+
+
+typedef struct
+{
+    int futurePrediction;
+    int accessFreq;
+    int pastPrediction;
+    int presentLater;
+} pageData;
 
 typedef struct
 {
@@ -202,6 +213,126 @@ void optPageReplacement(MMU *mmu, int pageNum, int refString[], int currentIdx)
 }
 
 
+void hpraPageReplacement(MMU *mmu, int pageNum, int refString[], int currentIdx)
+{
+    int foundReplacement = 0;
+    // Handle page fault and add the page to physical memory
+    int freeFrame = findFreeFrame(mmu);
+    if (freeFrame != -1)
+    {
+        LOG("Found a free frame for page number = %d, free frame = %d\n", pageNum, freeFrame);
+        mmu->page_table.entries[pageNum].valid = 1;
+        mmu->page_table.entries[pageNum].frame_number = freeFrame;
+        mmu->physical_memory.frame[freeFrame].pageNumber = pageNum;
+        mmu->physical_memory.frame[freeFrame].valid = 1;
+        return;
+    }
+
+    LOG("No free frame\n");
+    pageData pagesInFrame[NUM_FRAMES];
+    int checkPageNum;
+    int pageToReplace = 0;
+    int minPageFrequency = INT_MIN;
+    int leastDist = INT_MAX;
+
+    for(int i = 0; i < NUM_FRAMES; i++)
+    {
+        LOG("For frame [%d]\n", i);
+        pagesInFrame[i].futurePrediction = -1;
+        pagesInFrame[i].accessFreq = 0;
+        pagesInFrame[i].pastPrediction = -1;
+        pagesInFrame[i].presentLater = 0;
+        checkPageNum = mmu->physical_memory.frame[i].pageNumber; // Iterate through every page in the frame
+
+        for(int j = currentIdx + 1; j < PROC_PAGES; j++)
+        {
+            if(refString[j] == checkPageNum)
+            {
+                pagesInFrame[i].futurePrediction = j;
+                pagesInFrame[i].presentLater = 1;
+                LOG("The page %d is referenced later\n", checkPageNum);
+                break;
+            }
+        }
+
+        // Counter (frequency of page access)
+        for (int j = currentIdx + 1; j < PROC_PAGES; j++)
+        {
+            if (refString[j] == checkPageNum)
+            {
+                pagesInFrame[i].accessFreq++;
+            }
+        }
+
+        LOG("The number of times the page is present after this is %d\n", pagesInFrame[i].accessFreq);
+
+        // Past prediction
+        for(int j = currentIdx - 1; j >= 0; j--)
+        {
+            if(refString[j] == checkPageNum)
+            {
+                pagesInFrame[i].pastPrediction = j;
+                LOG("The page was present before at index %d\n", j);
+                break;
+            }
+        }
+    }
+
+
+    minPageFrequency = pagesInFrame[0].accessFreq;
+    LOG("The min page frequency to begin with is %d\n", minPageFrequency);
+
+    pageToReplace = 0;
+    LOG("Let's say the page to replace is %d\n", pageToReplace);
+
+    // To figure out page to be replaced, look at all the pages
+    // If the page is present later, then it shouldn't be replaced
+    for(int i = 0; i < NUM_FRAMES; i++)
+    {
+        if(pagesInFrame[i].futurePrediction == -1)
+        {
+            pageToReplace = i;
+            LOG("Page in frame %d won't be referenced later\n", pageToReplace);
+            foundReplacement = 1;
+            break;
+        }
+
+        // If you can't find a page for later, maintain counts of which page is accessed more
+        if(pagesInFrame[i].accessFreq <= minPageFrequency)
+        {
+            minPageFrequency = pagesInFrame[i].accessFreq;
+            pageToReplace = i;
+            LOG("Looks like min page frequency is now %d for frame %d\n", minPageFrequency, pageToReplace);
+            foundReplacement = 1;
+        }
+
+    }
+
+    if(!foundReplacement)
+    {
+        for( int i = 0; i < NUM_FRAMES; i++)
+        {
+            if((pagesInFrame[i].pastPrediction != -1) && ((currentIdx - pagesInFrame[i].pastPrediction) < leastDist))
+            {
+                leastDist = currentIdx - pagesInFrame[i].pastPrediction;
+                pageToReplace = i;
+                LOG("Frame [%d]: Current Idx is %d and least distance is %d so page to replace is %d\n", i, currentIdx, leastDist, pageToReplace);
+            }
+        }
+    }
+
+    int pageDel = mmu->physical_memory.frame[pageToReplace].pageNumber;
+    LOG("Did not find a free frame for page number = %d, so replacing page = %d\n", pageNum, pageToReplace);
+    mmu->page_table.entries[pageDel].valid = -1;
+    mmu->page_table.entries[pageNum].frame_number = pageToReplace;
+    mmu->page_table.entries[pageNum].valid = 1;
+    mmu->physical_memory.frame[pageToReplace].pageNumber = pageNum;
+    mmu->physical_memory.frame[pageToReplace].valid = 1;
+}
+
+
+
+
 void addPage(MMU *mmu, int pageNum, int refString[], int currentIdx)
 {
     if (pra == FIFO)
@@ -215,6 +346,10 @@ void addPage(MMU *mmu, int pageNum, int refString[], int currentIdx)
     else if (pra == OPT)
     {
         optPageReplacement(mmu, pageNum, refString, currentIdx);
+    }
+    else if (pra == HRPA)
+    {
+        hpraPageReplacement(mmu, pageNum, refString, currentIdx);
     }
 }
 
@@ -265,6 +400,10 @@ int main()
             LOG("YAY: Page hit for %d\n", pageNum);
             numPageHits++;
         }
+
+        printf("Current frame is %d %d %d %d\n", mmu.physical_memory.frame[0].pageNumber, 
+            mmu.physical_memory.frame[1].pageNumber, mmu.physical_memory.frame[2].pageNumber, 
+            mmu.physical_memory.frame[3].pageNumber);
     }
 
     LOG("Ultimately, what's there is:\n");
